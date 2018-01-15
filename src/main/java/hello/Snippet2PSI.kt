dfcd67d7aa6a51042a26fc794e7cfccc27cf296d
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2018-01-16 02:35:53 +0300

    Snippet to PSI.

---
+++ src/main/java/hello/Snippet2PSI.kt
@@ -0,0 +1,25 @@
+package hello
+
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    val folder = "..\\Snippets"
+
+    val listFoldersWithSnippets = File(folder).listFiles()
+
+    for (snipFolder in listFoldersWithSnippets) {
+        File(snipFolder.path).listFiles().forEach {
+            val path = it.path
+
+            if (!path.contains("PSI")) {
+                val result = ASTBuilder().buildAST(path)
+
+                val startName = path.lastIndexOf('s') + 1
+                val fileName = snipFolder.path + "\\" + "PSI" + path.substring(startName)
+
+                File(fileName).appendText(result)
+            }
+        }
+    }
+}
