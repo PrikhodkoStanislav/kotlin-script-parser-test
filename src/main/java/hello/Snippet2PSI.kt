bc5bf543944d15eec80779ecedc02ba445772e63
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2018-02-08 18:05:33 +0300

    Update processing exceptions.

--- src/main/java/hello/Snippet2PSI.kt
+++ src/main/java/hello/Snippet2PSI.kt
@@ -9,32 +9,45 @@ fun main(args: Array<String>) {
     val listFoldersWithSnippets = File(folder).listFiles()
 
     for (snipFolder in listFoldersWithSnippets) {
+        val snipPath = snipFolder.path
         try {
-            File(snipFolder.path).listFiles().forEach {
+            File(snipPath).listFiles().forEach {
                 val path = it.path
+                try {
 
-                if (!path.contains("PSI")) {
-                    val result = ASTBuilder().buildAST(path)
+                    if (!path.contains("PSI")) {
+                        val result = ASTBuilder().buildAST(path)
 
-                    val startName = path.lastIndexOf('s') + 1
+                        val startName = path.lastIndexOf('s') + 1
 
-                    //val fileName = snipFolder.path + "\\" + "PSI" + "\\" + "PSI" + path.substring(startName)
-                    //File(fileName).appendText(result)
+                        //val fileName = snipFolder.path + "\\" + "PSI" + "\\" + "PSI" + path.substring(startName)
+                        //File(fileName).appendText(result)
 
-                    val newDirectory = File(snipFolder.path + "\\" + "PSI" + "\\")
-                    newDirectory.mkdirs()
+                        val newDirectory = File(snipPath + "\\" + "PSI" + "\\")
+                        newDirectory.mkdirs()
 
-                    val fileName = "PSI" + path.substring(startName)
+                        val fileName = "PSI" + path.substring(startName)
 
-                    File(newDirectory, fileName).appendText(result)
+                        File(newDirectory, fileName).appendText(result)
+                    }
+                }
+                catch (e: Exception) {
+                    println(path)
+                    println(e.message)
+                }
+                catch(e: Error) {
+                    println(path)
+                    println(e.message)
                 }
             }
         }
         catch (e: Exception) {
-
+            println(snipPath)
+            println(e.message)
         }
         catch (e: Error) {
-
+            println(snipPath)
+            println(e.message)
         }
     }
 }
